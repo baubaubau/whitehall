5253a25dcfd16fcdec34abe55a55e2e4686e170f
Paul Bowsher <paul.bowsher@gmail.com>
2015-04-13 09:10:16 +0100

    Add bones of show action

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,7 +1,9 @@
 class CourtsController < ApplicationController
-
   def index
     @courts = Organisation.courts
   end
 
+  def show
+    @court = Organisation.courts.find_by! slug: params[:id]
+  end
 end
--- test/functional/courts_controller_test.rb
+++ test/functional/courts_controller_test.rb
@@ -12,6 +12,35 @@ class CourtsControllerTest < ActionController::TestCase
     court = create(:court, name: "High Court")
     get :index
 
-    assert_select "a[href=#{court_path(court)}]"
+    assert_select "a[href=#{court_path(court)}]", text: "High Court"
+  end
+
+  test "show assigns @court as requested Court" do
+    court = create(:court)
+    get :show, id: court
+    assert_equal court, assigns(:court)
+  end
+
+  test "show renders a 404 if a non-Court organisation is requested" do
+    org = create(:organisation)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get :show, id: org
+    end
+  end
+
+  view_test "show is headed and titled with the name of the Court" do
+    court = create(:court, name: "High Court")
+    get :show, id: court
+    assert_select "h1", text: "High Court"
+    assert_select "title", text: "High Court - GOV.UK"
+  end
+
+  view_test "show renders the 'About Us' summary" do
+    court = create(:court, name: "High Court")
+    about_us = create(:about_corporate_information_page,
+                      summary: "High Court is a pretty high Court",
+                      organisation: court)
+    get :show, id: court
+    assert_select ".what-we-do", text: "High Court is a pretty high Court"
   end
 end
