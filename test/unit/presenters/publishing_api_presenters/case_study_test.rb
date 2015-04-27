b2376d4637b426cc09e5c9d555889d95c042454a
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-04-27 12:39:01 +0100

    Include new policy taggings in content store hash.
    This will be used to send emails alerts via the
    newer system.  We only need send new taggings
    because all systems will be switched over at the
    same time, and the taggings migrated.

--- test/unit/presenters/publishing_api_presenters/case_study_test.rb
+++ test/unit/presenters/publishing_api_presenters/case_study_test.rb
@@ -38,7 +38,8 @@ class PublishingApiPresenters::CaseStudyTest < ActiveSupport::TestCase
         ],
         tags: {
           browse_pages: [],
-          topics: []
+          topics: [],
+          policies: []
         }
       },
       links: {
